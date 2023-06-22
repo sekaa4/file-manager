12aa56a3536829e45674a088b58c533f0c17bfb7
Sergey P <s.pansevi4@gmail.com>
2023-06-22 13:49:08 +0300

    feat: add parseCommandLine function

---
+++ src/utils/parseCommandLine.js
@@ -0,0 +1,22 @@
+const flagsObj = {
+  navigation: ['cd', 'up', 'ls'],
+  basic: ['cat', 'add', 'rn', 'rm', 'cp', 'mv'],
+  system: ['os'],
+  hash: ['hash'],
+  archive: ['compress', 'decompress'],
+  exit: ['.exit']
+};
+
+const chooseFlag = (command) => {
+  const flag = Object.keys(flagsObj).find(flag => flagsObj[flag].includes(command));
+  return flag;
+};
+
+export const parseCommandLine = (data) => {
+  const commandLine = data.trim();
+  if (!commandLine) return { command: '', args: [], flag: 'unknown' };
+  const [command, ...args] = commandLine.split(' ');
+  const flag = chooseFlag(command);
+
+  return { command, args, flag: flag || 'unknown' };
+};

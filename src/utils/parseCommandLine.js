839912b6a84cb1e5ede372e970bb7e0f1a499082
Sergey P <s.pansevi4@gmail.com>
2023-06-23 21:54:39 +0300

    fix: change behavior parseCommandLine function, for work with filename with whitespace

--- src/utils/parseCommandLine.js
+++ src/utils/parseCommandLine.js
@@ -12,11 +12,51 @@ const chooseFlag = (command) => {
   return flag;
 };
 
+const buildArgs = (args) => {
+  const stack = [];
+  let str = '';
+
+  const parsedArgs = args.reduce((acc, cur, curIndex) => {
+    let flag = false;
+
+    if (cur === '\'' && !stack.length) {
+      stack.push(cur);
+      flag = true;
+    }
+
+    if (cur === '\'' && stack.length && !flag) {
+      stack.pop();
+    }
+
+    if (cur === ' ' && !stack.length) {
+      acc.push(str);
+      str = '';
+      return acc;
+    }
+
+    str += cur;
+    if (curIndex === args.length - 1) {
+      acc.push(str);
+    }
+    return acc;
+  }, []);
+
+  return parsedArgs;
+};
+
 export const parseCommandLine = (data) => {
   const commandLine = data.trim();
   if (!commandLine) return { command: '', args: [], flag: 'unknown' };
   const [command, ...args] = commandLine.split(' ');
-  const curArgs = args.length ? args.join(' ').trim().split(' ') : args;
+  let curArgs = args;
+
+  if (args.length) {
+    const curSplitArgs = args.join(' ').trim().replaceAll('\"', '\'').split('');
+    const isExistQuote = curSplitArgs.some((arg) => arg.includes('\''));
+
+    if (isExistQuote) curArgs = buildArgs(curSplitArgs);
+  }
+
   const flag = chooseFlag(command);
 
   return { command, args: curArgs, flag: flag || 'unknown' };
